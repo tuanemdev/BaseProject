dcec5d53f9090e2ee1a2c3f2d931eaa594d00939
tuanemdev <104725942+tuanemdev@users.noreply.github.com>
2023-12-24 10:31:12 +0700

    Initial Commit

---
+++ BaseProject/BaseProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaseProjectApp.swift
+//  BaseProject
+//
+//  Created by Nguyen Tuan Anh on 24/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct BaseProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
